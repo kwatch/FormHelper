0a4fb0c11871860130d46b5286f7b05909e6e48a
makoto kuwata <kwa@kuwata-lab.com>
2012-03-25 17:55:31 +0900

    python: comment out 'FormHelper' class

--- lib/formhelper.py
+++ lib/formhelper.py
@@ -3,9 +3,9 @@
 from collections import OrderedDict
 from markupsafe import Markup, escape
 
-
-UNDEFINED = object()
-
+#|
+#|UNDEFINED = object()
+#|
 
 class Form(object):
 
@@ -69,70 +69,70 @@ class FormItem(object):
         return Markup('value="%s"%s' % (escape(value), selected))
 
 
-class FormHelper(object):
-
-    _ERR_EXIST = Markup('class="err-exist"')
-
-    def __init__(self, form):
-        self._form = form
-        self._param_stack = []
-
-    def __enter__(self):
-        return self
-
-    def __exit__(self, *args):
-        self._param_stack.pop()
-
-    def param(self, name):
-        self._param_stack.append(name)
-        return self
-
-    @property
-    def last_param(self):
-        return self._param_stack[-1]
-
-    def ec(self, name=None):
-        """returns 'class="err-exist"' when form parameter has error."""
-        if name is None: name = self.last_param
-        return self._form.errors.get(name) and self._ERR_EXIST or ''
-
-    def em(self, name=None, emclass='err-desc'):
-        """returns '<em class="err-desc">MESSAGE</em>' when form parameter has error."""
-        if name is None: name = self.last_param
-        errmsg = self._form.errors.get(name)
-        if errmsg:
-            return Markup('<em class="%s">%s</em>' % (emclass, escape(errmsg),))
-        return ''
-
-    def nv(self, name=None):
-        """returns ' name="..." value="..."'."""
-        if name is None: name = self.last_param
-        value = self._form.params.get(name, '')
-        return Markup('name="%s" value="%s"' % (escape(name), escape(value)))
-
-    def nvc(self, name, value=UNDEFINED):
-        """returns 'name="..." value="..." checked="checked"'."""
-        if value is UNDEFINED:
-            value = name
-            name  = self.last_param
-        checked = self._form.params.get(name, '') == value
-        s = checked and ' checked="checked"' or ''
-        return Markup('name="%s" value="%s"%s' % (escape(name), escape(value), s))
-
-
-def checked(boolean):
-    """returns 'checked="checked"' if boolean is truthy value."""
-    return boolean and _CHECKED or ''
-
-def selected(boolean):
-    """returns 'selected="selected"' if boolean is truthy value."""
-    return boolean and _SELECTED or ''
-
-def disabled(boolean):
-    """returns 'disabled="disabled"' if boolean is truthy value."""
-    return boolean and _DISABLED or ''
-
-
-_CHECKED  = Markup('checked="checked"')
-_SELECTED = Markup('selected="selected"')
-_DISABLED = Markup('disabled="disabled"')
+#|class FormHelper(object):
+#|
+#|    _ERR_EXIST = Markup('class="err-exist"')
+#|
+#|    def __init__(self, form):
+#|        self._form = form
+#|        self._param_stack = []
+#|
+#|    def __enter__(self):
+#|        return self
+#|
+#|    def __exit__(self, *args):
+#|        self._param_stack.pop()
+#|
+#|    def param(self, name):
+#|        self._param_stack.append(name)
+#|        return self
+#|
+#|    @property
+#|    def last_param(self):
+#|        return self._param_stack[-1]
+#|
+#|    def ec(self, name=None):
+#|        """returns 'class="err-exist"' when form parameter has error."""
+#|        if name is None: name = self.last_param
+#|        return self._form.errors.get(name) and self._ERR_EXIST or ''
+#|
+#|    def em(self, name=None, emclass='err-desc'):
+#|        """returns '<em class="err-desc">MESSAGE</em>' when form parameter has error."""
+#|        if name is None: name = self.last_param
+#|        errmsg = self._form.errors.get(name)
+#|        if errmsg:
+#|            return Markup('<em class="%s">%s</em>' % (emclass, escape(errmsg),))
+#|        return ''
+#|
+#|    def nv(self, name=None):
+#|        """returns ' name="..." value="..."'."""
+#|        if name is None: name = self.last_param
+#|        value = self._form.params.get(name, '')
+#|        return Markup('name="%s" value="%s"' % (escape(name), escape(value)))
+#|
+#|    def nvc(self, name, value=UNDEFINED):
+#|        """returns 'name="..." value="..." checked="checked"'."""
+#|        if value is UNDEFINED:
+#|            value = name
+#|            name  = self.last_param
+#|        checked = self._form.params.get(name, '') == value
+#|        s = checked and ' checked="checked"' or ''
+#|        return Markup('name="%s" value="%s"%s' % (escape(name), escape(value), s))
+#|
+#|
+#|def checked(boolean):
+#|    """returns 'checked="checked"' if boolean is truthy value."""
+#|    return boolean and _CHECKED or ''
+#|
+#|def selected(boolean):
+#|    """returns 'selected="selected"' if boolean is truthy value."""
+#|    return boolean and _SELECTED or ''
+#|
+#|def disabled(boolean):
+#|    """returns 'disabled="disabled"' if boolean is truthy value."""
+#|    return boolean and _DISABLED or ''
+#|
+#|
+#|_CHECKED  = Markup('checked="checked"')
+#|_SELECTED = Markup('selected="selected"')
+#|_DISABLED = Markup('disabled="disabled"')
--- test/formhelper_test.py
+++ test/formhelper_test.py
@@ -5,7 +5,7 @@ from bakatest import ok, prepare, topic, spec, fixture
 prepare()
 
 from markupsafe import Markup, escape
-from formhelper import Form, FormItem, FormHelper, checked, selected, disabled
+from formhelper import Form, FormItem
 
 
 with topic('Form'):
@@ -207,192 +207,192 @@ with topic('FormItem'):
             ok (fi.vs(1)) == Markup('value="1" selected="selected"')
 
 
-class HelloForm(Form):
-
-    def validate(self):
-        k = 'name'
-        v = self.params.get(k, '').strip()
-        if not v:
-            self.errors[k] = 'Required.'
-        elif len(v) > 100:
-            self.errors[k] = 'Too long (max 100 chars).'
-
-
-with topic('FormHelper'):
-
-
-    with topic('#__init__()'):
-
-        @spec("takes form object.")
-        def _(self):
-            form = Form({})
-            fh = FormHelper(form)
-            ok (fh._form).is_(form)
-
-
-    @fixture
-    def fx_valid_form(self):
-        form = HelloForm({'name': 'Haruhi'})
-        form.validate()
-        return form
-
-    @fixture
-    def fx_invalid_form(self):
-        form = HelloForm({'name': ''})
-        form.validate()
-        return form
-
-    @fixture
-    def fx_valid_form_helper(self, valid_form):
-        return FormHelper(valid_form)
-
-    @fixture
-    def fx_invalid_form_helper(self, invalid_form):
-        return FormHelper(invalid_form)
-
-
-    with topic('#param()'):
-
-        @spec("push/pop parameter name.")
-        def _(self, valid_form_helper):
-            fh = valid_form_helper
-            with fh.param('name'):
-                with fh.param('age'):
-                    with fh.param('birthday'):
-                        fh._param_stack == ['name', 'age', 'birthday']
-                    fh._param_stack == ['name', 'age']
-                fh._param_stack == ['name']
-            fh._param_stack == []
-
-
-    with topic('#ec()'):
-
-        @spec("returns empty string when parameter has no error.")
-        def _(self, valid_form_helper):
-            fh = valid_form_helper
-            ok (fh.ec('name')) == ''
-
-        @spec("returns 'class=\"err-exist\"' when parameter has error.")
-        def _(self, invalid_form_helper):
-            fh = invalid_form_helper
-            ok (fh.ec('name')) == Markup('class="err-exist"')
-
-        @spec("param name is optional when #param() is called before.")
-        def _(self, valid_form_helper, invalid_form_helper):
-            fh = valid_form_helper
-            with fh.param('name'):
-                ok (fh.ec()) == ''
-            #
-            fh = invalid_form_helper
-            with fh.param('name'):
-                ok (fh.ec()) == Markup('class="err-exist"')
-
-
-    with topic('#em()'):
-
-        @spec("returns empty string when parameter has no error.")
-        def _(self, valid_form_helper):
-            fh = valid_form_helper
-            ok (fh.em('name')) == ''
-
-        @spec("returns '<em class=\"err-desc\">MESSAgE</em>' when parameter has error.")
-        def _(self, invalid_form_helper):
-            fh = invalid_form_helper
-            errmsg = invalid_form_helper._form.errors.get('name')
-            ok (errmsg) == 'Required.'
-            ok (fh.em('name')) == Markup('<em class="err-desc">%s</em>' % errmsg)
-
-        @spec("param name is optional when #param() is called before.")
-        def _(self, valid_form_helper, invalid_form_helper):
-            fh = valid_form_helper
-            with fh.param('name'):
-                ok (fh.em()) == ''
-            #
-            fh = invalid_form_helper
-            errmsg = fh._form.errors.get('name')
-            with fh.param('name'):
-                ok (fh.em()) == Markup('<em class="err-desc">%s</em>' % errmsg)
-
-
-    with topic('#nv()'):
-
-        @spec("returns 'name=\"...\" value=\"...\"' string.")
-        def _(self, valid_form_helper, invalid_form_helper):
-            ok (  valid_form_helper.nv('name')) == Markup('name="name" value="Haruhi"')
-            ok (invalid_form_helper.nv('name')) == Markup('name="name" value=""')
-
-        @spec("param name is optional when #param() is called before.")
-        def _(self, valid_form_helper, invalid_form_helper):
-            with valid_form_helper.param('name'):
-                ok (  valid_form_helper.nv()) == Markup('name="name" value="Haruhi"')
-            with invalid_form_helper.param('name'):
-                ok (invalid_form_helper.nv()) == Markup('name="name" value=""')
-
-
-    with topic('#nvc()'):
-
-        @spec("returns attributes without 'checked' when parameter is different from value.")
-        def _(self, valid_form_helper, valid_form):
-            valid_form.params['radio1'] = ''
-            ok (valid_form_helper.nvc('radio1', 'val1')) == Markup('name="radio1" value="val1"')
-            valid_form.params['radio1'] = 'xxx'
-            ok (valid_form_helper.nvc('radio1', 'val1')) == Markup('name="radio1" value="val1"')
-
-        @spec("returns attributes with 'checked' when parameter equals to value.")
-        def _(self, valid_form_helper, valid_form):
-            valid_form.params['radio2'] = 'val2'
-            ok (valid_form_helper.nvc('radio2', 'val2')) == Markup('name="radio2" value="val2" checked="checked"')
-
-        @spec("param name is optional when #param() is called before.")
-        def _(self, valid_form, valid_form_helper, invalid_form, invalid_form_helper):
-            fh = valid_form_helper
-            valid_form.params['radio1'] = ''
-            with fh.param('radio1'):
-                ok (fh.nvc('val1')) == Markup('name="radio1" value="val1"')
-            valid_form.params['radio1'] = 'xxx'
-            with fh.param('radio1'):
-                ok (fh.nvc('val1')) == Markup('name="radio1" value="val1"')
-            #
-            fh = invalid_form_helper
-            invalid_form.params['radio2'] = 'val2'
-            with fh.param('radio2'):
-                ok (fh.nvc('val2')) == Markup('name="radio2" value="val2" checked="checked"')
-
-
-with topic('<global>'):
-
-
-    with topic('checked()'):
-
-        @spec("returns empty string when argument is falthy value.")
-        def _():
-            ok (checked(1==0)) == ''
-
-        @spec("returns 'checked=\"checked\"' when argument is truthy value.")
-        def _():
-            ok (checked(1==1)) == Markup('checked="checked"')
-
-
-    with topic('selected()'):
-
-        @spec("returns empty string when argument is falthy value.")
-        def _():
-            ok (selected(1==0)) == ''
-
-        @spec("returns 'selected=\"selected\"' when argument is truthy value.")
-        def _():
-            ok (selected(1==1)) == Markup('selected="selected"')
-
-
-    with topic('disabled()'):
-
-        @spec("returns empty string when argument is falthy value.")
-        def _():
-            ok (disabled(1==0)) == ''
-
-        @spec("returns 'disabled=\"disabled\"' when argument is truthy value.")
-        def _():
-            ok (disabled(1==1)) == Markup('disabled="disabled"')
+#|class HelloForm(Form):
+#|
+#|    def validate(self):
+#|        k = 'name'
+#|        v = self.params.get(k, '').strip()
+#|        if not v:
+#|            self.errors[k] = 'Required.'
+#|        elif len(v) > 100:
+#|            self.errors[k] = 'Too long (max 100 chars).'
+#|
+#|
+#|with topic('FormHelper'):
+#|
+#|
+#|    with topic('#__init__()'):
+#|
+#|        @spec("takes form object.")
+#|        def _(self):
+#|            form = Form({})
+#|            fh = FormHelper(form)
+#|            ok (fh._form).is_(form)
+#|
+#|
+#|    @fixture
+#|    def fx_valid_form(self):
+#|        form = HelloForm({'name': 'Haruhi'})
+#|        form.validate()
+#|        return form
+#|
+#|    @fixture
+#|    def fx_invalid_form(self):
+#|        form = HelloForm({'name': ''})
+#|        form.validate()
+#|        return form
+#|
+#|    @fixture
+#|    def fx_valid_form_helper(self, valid_form):
+#|        return FormHelper(valid_form)
+#|
+#|    @fixture
+#|    def fx_invalid_form_helper(self, invalid_form):
+#|        return FormHelper(invalid_form)
+#|
+#|
+#|    with topic('#param()'):
+#|
+#|        @spec("push/pop parameter name.")
+#|        def _(self, valid_form_helper):
+#|            fh = valid_form_helper
+#|            with fh.param('name'):
+#|                with fh.param('age'):
+#|                    with fh.param('birthday'):
+#|                        fh._param_stack == ['name', 'age', 'birthday']
+#|                    fh._param_stack == ['name', 'age']
+#|                fh._param_stack == ['name']
+#|            fh._param_stack == []
+#|
+#|
+#|    with topic('#ec()'):
+#|
+#|        @spec("returns empty string when parameter has no error.")
+#|        def _(self, valid_form_helper):
+#|            fh = valid_form_helper
+#|            ok (fh.ec('name')) == ''
+#|
+#|        @spec("returns 'class=\"err-exist\"' when parameter has error.")
+#|        def _(self, invalid_form_helper):
+#|            fh = invalid_form_helper
+#|            ok (fh.ec('name')) == Markup('class="err-exist"')
+#|
+#|        @spec("param name is optional when #param() is called before.")
+#|        def _(self, valid_form_helper, invalid_form_helper):
+#|            fh = valid_form_helper
+#|            with fh.param('name'):
+#|                ok (fh.ec()) == ''
+#|            #
+#|            fh = invalid_form_helper
+#|            with fh.param('name'):
+#|                ok (fh.ec()) == Markup('class="err-exist"')
+#|
+#|
+#|    with topic('#em()'):
+#|
+#|        @spec("returns empty string when parameter has no error.")
+#|        def _(self, valid_form_helper):
+#|            fh = valid_form_helper
+#|            ok (fh.em('name')) == ''
+#|
+#|        @spec("returns '<em class=\"err-desc\">MESSAgE</em>' when parameter has error.")
+#|        def _(self, invalid_form_helper):
+#|            fh = invalid_form_helper
+#|            errmsg = invalid_form_helper._form.errors.get('name')
+#|            ok (errmsg) == 'Required.'
+#|            ok (fh.em('name')) == Markup('<em class="err-desc">%s</em>' % errmsg)
+#|
+#|        @spec("param name is optional when #param() is called before.")
+#|        def _(self, valid_form_helper, invalid_form_helper):
+#|            fh = valid_form_helper
+#|            with fh.param('name'):
+#|                ok (fh.em()) == ''
+#|            #
+#|            fh = invalid_form_helper
+#|            errmsg = fh._form.errors.get('name')
+#|            with fh.param('name'):
+#|                ok (fh.em()) == Markup('<em class="err-desc">%s</em>' % errmsg)
+#|
+#|
+#|    with topic('#nv()'):
+#|
+#|        @spec("returns 'name=\"...\" value=\"...\"' string.")
+#|        def _(self, valid_form_helper, invalid_form_helper):
+#|            ok (  valid_form_helper.nv('name')) == Markup('name="name" value="Haruhi"')
+#|            ok (invalid_form_helper.nv('name')) == Markup('name="name" value=""')
+#|
+#|        @spec("param name is optional when #param() is called before.")
+#|        def _(self, valid_form_helper, invalid_form_helper):
+#|            with valid_form_helper.param('name'):
+#|                ok (  valid_form_helper.nv()) == Markup('name="name" value="Haruhi"')
+#|            with invalid_form_helper.param('name'):
+#|                ok (invalid_form_helper.nv()) == Markup('name="name" value=""')
+#|
+#|
+#|    with topic('#nvc()'):
+#|
+#|        @spec("returns attributes without 'checked' when parameter is different from value.")
+#|        def _(self, valid_form_helper, valid_form):
+#|            valid_form.params['radio1'] = ''
+#|            ok (valid_form_helper.nvc('radio1', 'val1')) == Markup('name="radio1" value="val1"')
+#|            valid_form.params['radio1'] = 'xxx'
+#|            ok (valid_form_helper.nvc('radio1', 'val1')) == Markup('name="radio1" value="val1"')
+#|
+#|        @spec("returns attributes with 'checked' when parameter equals to value.")
+#|        def _(self, valid_form_helper, valid_form):
+#|            valid_form.params['radio2'] = 'val2'
+#|            ok (valid_form_helper.nvc('radio2', 'val2')) == Markup('name="radio2" value="val2" checked="checked"')
+#|
+#|        @spec("param name is optional when #param() is called before.")
+#|        def _(self, valid_form, valid_form_helper, invalid_form, invalid_form_helper):
+#|            fh = valid_form_helper
+#|            valid_form.params['radio1'] = ''
+#|            with fh.param('radio1'):
+#|                ok (fh.nvc('val1')) == Markup('name="radio1" value="val1"')
+#|            valid_form.params['radio1'] = 'xxx'
+#|            with fh.param('radio1'):
+#|                ok (fh.nvc('val1')) == Markup('name="radio1" value="val1"')
+#|            #
+#|            fh = invalid_form_helper
+#|            invalid_form.params['radio2'] = 'val2'
+#|            with fh.param('radio2'):
+#|                ok (fh.nvc('val2')) == Markup('name="radio2" value="val2" checked="checked"')
+#|
+#|
+#|with topic('<global>'):
+#|
+#|
+#|    with topic('checked()'):
+#|
+#|        @spec("returns empty string when argument is falthy value.")
+#|        def _():
+#|            ok (checked(1==0)) == ''
+#|
+#|        @spec("returns 'checked=\"checked\"' when argument is truthy value.")
+#|        def _():
+#|            ok (checked(1==1)) == Markup('checked="checked"')
+#|
+#|
+#|    with topic('selected()'):
+#|
+#|        @spec("returns empty string when argument is falthy value.")
+#|        def _():
+#|            ok (selected(1==0)) == ''
+#|
+#|        @spec("returns 'selected=\"selected\"' when argument is truthy value.")
+#|        def _():
+#|            ok (selected(1==1)) == Markup('selected="selected"')
+#|
+#|
+#|    with topic('disabled()'):
+#|
+#|        @spec("returns empty string when argument is falthy value.")
+#|        def _():
+#|            ok (disabled(1==0)) == ''
+#|
+#|        @spec("returns 'disabled=\"disabled\"' when argument is truthy value.")
+#|        def _():
+#|            ok (disabled(1==1)) == Markup('disabled="disabled"')
 
 
 
