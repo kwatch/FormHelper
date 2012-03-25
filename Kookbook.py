b01b304b5f3223b29dfc4381e2bacd00b7b74dec
makoto kuwata <kwa@kuwata-lab.com>
2012-03-25 17:33:52 +0900

    python: add 'html' task

--- Kookbook.py
+++ Kookbook.py
@@ -8,4 +8,19 @@ class test(Category):
 
     @recipe
     def default(c):
+        """do test"""
         system(c%"baka.py -ss test")
+
+
+@recipe('*.html')
+@ingreds('$(1).py', '$(1).pyhtml')
+def task_html(c):
+    """generates *.html from *.py and *.pyhtml"""
+    system(c%"python $(ingred) > $(product)")
+
+
+@recipe
+@ingreds('example1.html')
+def task_html(c):
+    """generates example1.html"""
+    pass
